0b9457e4e0741dd65fcfd175ad67487d741b7b47
Vinicius Alves <103587422+Viniciusalves16@users.noreply.github.com>
2025-04-30 20:23:13 -0300

    Iniciando desenvolvimento app

---
+++ PaySwift/src/main/java/com/api/PaySwift/PaySwiftApplication.java
@@ -0,0 +1,14 @@
+package com.api.PaySwift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaySwiftApplication {
+
+	public static void main(String[] args) {
+		System.out.println("iniciando aplicaçao");
+		SpringApplication.run(PaySwiftApplication.class, args);
+	}
+
+}
---
+++ PaySwift/src/test/java/com/api/PaySwift/PaySwiftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.PaySwift;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaySwiftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
