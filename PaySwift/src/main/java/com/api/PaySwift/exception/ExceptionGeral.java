ee87beaeb0c475ab1aaaab6798750e12eb4cb54b
Vinicius Alves <103587422+Viniciusalves16@users.noreply.github.com>
2025-05-01 16:57:51 -0300

    Incluindo classe ExceptionGeral responsavel pelo tratamento do erro bad request 400 em casos de uma requisicao nao atender os requisitos

---
+++ PaySwift/src/main/java/com/api/PaySwift/exception/ExceptionGeral.java
@@ -0,0 +1,29 @@
+package com.api.PaySwift.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class ExceptionGeral {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity badRequest400(MethodArgumentNotValidException exception) {
+        var errors = exception.getFieldErrors();
+        return ResponseEntity.badRequest().body(errors.stream().map(errorValidation::new).toList());
+    }
+
+    private record errorValidation(String atribute, String message) {
+        public errorValidation(FieldError error) {
+            this(error.getField(), error.getDefaultMessage());
+        }
+    }
+
+}
