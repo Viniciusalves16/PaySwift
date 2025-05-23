378af2f6d950a3a8cdcdee730f65738495f64d00
Vinicius Alves <103587422+Viniciusalves16@users.noreply.github.com>
2025-05-23 16:10:05 -0300

    Alterando injeção de depedencia para o tipo final para garantir a imutabilidade ✅

--- PaySwift/src/main/java/com/api/PrePaymentAPI/service/PaymentIntegrationService.java
+++ PaySwift/src/main/java/com/api/PrePaymentAPI/service/PaymentIntegrationService.java
@@ -4,19 +4,17 @@ import com.api.PrePaymentAPI.dto.PaymentResponseDTO;
 import com.api.PrePaymentAPI.dto.RequestPaymentDto;
 import com.api.PrePaymentAPI.dto.RequestTopicPayment;
 import com.api.PrePaymentAPI.repository.UserRepository;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
-import java.util.Random;
 import java.util.UUID;
 
 @Service
 public class PaymentIntegrationService {
 
-    private KafkaTemplate <String, String>kafkaTemplate;
-    private UserRepository userRepository;
+    private final  KafkaTemplate <String, String>kafkaTemplate;
+    private final  UserRepository userRepository;
 
     public PaymentIntegrationService(KafkaTemplate<String, String> kafkaTemplate, UserRepository userRepository) {
         this.kafkaTemplate = kafkaTemplate;
@@ -32,13 +30,13 @@ public class PaymentIntegrationService {
         String id = paymentId.toString();
 
         RequestTopicPayment requestTopicPayment = new RequestTopicPayment(id, request.value(), request.paymentType().toString(), request.clientDetails().name());
-        this.ProducerPaymentTopic(requestTopicPayment);
+        this.sendPaymentToTopic(requestTopicPayment);
 
 
         return ResponseEntity.accepted().body(new PaymentResponseDTO(String.valueOf(requestTopicPayment.getPaymentId()), "PENDING"));
     }
 
-    public Object ProducerPaymentTopic(RequestTopicPayment request){
+    public Object sendPaymentToTopic(RequestTopicPayment request){
         return kafkaTemplate.send("Payment_Topic", String.valueOf(request));
 
     }
