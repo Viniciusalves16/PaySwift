b2f0fc3fa45a41f629a46c3a3a93c7ca210305cc
Vinicius Alves <103587422+Viniciusalves16@users.noreply.github.com>
2025-05-23 14:24:02 -0300

    Implementado Kafka - postando mensagem no tópico Payment_Topic ✅

--- PaySwift/src/main/java/com/api/PrePaymentAPI/PrePaymentApi.java
+++ PaySwift/src/main/java/com/api/PrePaymentAPI/PrePaymentApi.java
@@ -8,7 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PrePaymentApi {
 
 	public static void main(String[] args) {
-		System.out.println("iniciando aplicaçao");
 		SpringApplication.run(PrePaymentApi.class, args);
 	}
 
